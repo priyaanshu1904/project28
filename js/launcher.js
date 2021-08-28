85de2034615b104850c61fb833c0ede116dad0ca
priyaanshu1904 <83572369+priyaanshu1904@users.noreply.github.com>
2021-08-28 21:12:41 +0530

    Add files via upload

---
+++ js/launcher.js
@@ -0,0 +1,31 @@
+class launcher{
+    constructor(body,anchor){
+        var options={
+            bodyA:body,
+            pointB:anchor,
+            stiffness:0.004,
+            length:1
+        }
+        this.bodyA=body
+        this.pointB=anchor 
+        this.launcher=Constraint.create(options);
+        World.add(world,this.launcher)
+
+    }
+    display(){
+        if(this.launcher.bodyA){
+            var pointA=this.bodyA.position;
+            var pointB=this.pointB;
+            strokeWeight(2);
+            line(pointA.x,pointA.y,pointB.x,pointB.y);
+    
+        }
+
+    }
+    fly(){
+        this.launcher.bodyA=null;
+    }
+    attach(body){
+        this.launcher.bodyA=body;
+    }
+}
